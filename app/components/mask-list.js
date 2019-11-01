b8c5ae1fdda5af44981dff5ca3d4c81292d3eaa0
Chazzers <chazz@henryhandsome.nl>
2019-11-01 10:36:28 +0100

    deploy

--- app/components/mask-list.js
+++ app/components/mask-list.js
@@ -4,7 +4,6 @@ import { set } from '@ember/object';
 export default Component.extend({
 	actions: {
 		setMask(mask) {
-			console.log(set(this, 'someData', mask.picture));
 			return set(this, 'someData', mask.picture);
 		}
 	}
